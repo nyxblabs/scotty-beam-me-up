8f91ee3b2181577491b0131216fdafd5bc44c265
nyxb <contact@nyxb.xyz>
2023-05-18 21:59:13 +0200

    🔧 chore(.eslintrc): disable @next/next/no-html-link-for-pages rule and set React version to 18 ✅ test(index.test.ts, utils.ts): add tests for getPort function and create utility functions to block ports The @next/next/no-html-link-for-pages rule is disabled to allow the use of HTML links in Next.js pages. The React version is set to 18 to ensure compatibility with the latest version of React. Tests are added to ensure the getPort function works as expected. Utility functions are added to block ports for testing purposes.

---
+++ test/index.test.ts
@@ -0,0 +1,65 @@
+import type { Server } from 'node:net'
+import { afterEach, describe, expect, test } from 'vitest'
+import { getPort } from '../src'
+import { blockPort } from './utils'
+
+describe('getPort ()', () => {
+   describe('checks ports on default host`', () => {
+      let portBlocker: Server
+
+      afterEach(() => {
+         portBlocker?.close()
+      })
+
+      test('default port is NOT IN USE', async () => {
+         const port = await getPort()
+         expect(port).toEqual(3000)
+      })
+
+      test('default port is in use', async () => {
+         portBlocker = await blockPort(3000)
+         const port = await getPort()
+         expect(port).toEqual(3001)
+      })
+   })
+})
+
+describe('getPort (host)', () => {
+   describe('checks ports on `localhost`', () => {
+      let portBlocker: Server
+
+      afterEach(() => {
+         portBlocker?.close()
+      })
+
+      test('default port is NOT IN USE', async () => {
+         const port = await getPort({ host: 'localhost' })
+         expect(port).toEqual(3000)
+      })
+
+      test('default port is IN USE', async () => {
+         portBlocker = await blockPort(3000, 'localhost')
+         const port = await getPort({ host: 'localhost' })
+         expect(port).toEqual(3001)
+      })
+   })
+})
+
+describe('getPort (host)', () => {
+   let portBlocker: Server
+
+   afterEach(() => {
+      portBlocker?.close()
+   })
+
+   test('default port is in use', async () => {
+      process.env.HOST = 'localhost'
+      portBlocker = await blockPort(3000, 'localhost')
+      const port1 = await getPort({ port: 3000, portRange: [3000, 3100] })
+      expect(port1).toEqual(3001)
+      const port2 = await getPort()
+      expect(port2).toEqual(3001)
+      const port3 = await getPort(3000)
+      expect(port3).not.toEqual(3001)
+   })
+})
---
+++ test/utils.ts
@@ -0,0 +1,25 @@
+import type { Server } from 'node:net'
+import { createServer } from 'node:net'
+
+export function blockPort(port: number, host = '0.0.0.0'): Promise<Server> {
+   return new Promise((resolve) => {
+      const blocker = createServer()
+      blocker.listen(port, host, () => {
+         resolve(blocker)
+      })
+   })
+}
+
+export async function blockPorts(
+   ports: number[],
+   host = '0.0.0.0',
+): Promise<Server[]> {
+   const portBlockers: Server[] = []
+
+   for (const port of ports) {
+      const blocker = await blockPort(port, host)
+      portBlockers.push(blocker)
+   }
+
+   return portBlockers
+}
